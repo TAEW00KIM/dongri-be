a479c379f483c311f082cf54cd2aa483d6449c84
TAEW00KIM <twkk0819@hufs.ac.kr>
2025-10-30 13:06:44 +0900

    feat: Initial commit for Dongri

---
+++ src/main/java/com/hufs/dongri/DongriApplication.java
@@ -0,0 +1,13 @@
+package com.hufs.dongri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hufs/dongri/DongriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufs.dongri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
